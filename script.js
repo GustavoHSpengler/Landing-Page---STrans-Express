183b38adcc9a9c8cffcbe689a995733e3983dedb
GUSTAVO HENZ SPENGLER <05291874007@senacrs.edu.br>
2024-04-19 10:40:24 -0300

    Atualizado

---
+++ script.js
@@ -0,0 +1,24 @@
+let button = document.getElementById("envio")
+
+button.onclick = async function() {
+    let name = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let cellphone = document.getElementById("telefone").value;
+    let password = document.getElementById("senha").value;
+    let cpf = document.getElementById("cpf").value;
+    let data = {name, email, cellphone, password, cpf}
+
+    const response = await fetch("http://localhost:3005/api/store/task", {
+        method: "POST",
+        headers: {"Content-type": "application/json;charset=UTF-8"},
+        body: JSON.stringify(data)
+    });
+
+    let content = await response.json();
+
+    if (content.sucess) {
+        alert("Sucesso")
+    } else {
+        alert("Não")
+    }
+}
